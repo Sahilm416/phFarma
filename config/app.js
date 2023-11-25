185355910251afd040b93e142b144136deca2828
sahil <sahilmulani501@gmail.com>
2023-11-25 20:06:25 +0530

    initial commit

---
+++ config/app.js
@@ -0,0 +1,18 @@
+function nextContainer() {
+    document.getElementById("container1").style.display = "none";
+    document.getElementById("container2").style.display = "block";
+}
+
+function prevContainer() {
+    document.getElementById("container2").style.display = "none";
+    document.getElementById("container1").style.display = "block";
+}
+function logFormValues() {
+    // Get form values
+   
+    // Construct a data object
+   
+
+    
+
+}

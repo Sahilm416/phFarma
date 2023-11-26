f2d3cb75cb6abc8293bf76d1b7e157fe30f30b7f
sahil <114013781+Sahilm416@users.noreply.github.com>
2023-11-26 12:45:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase
 import { initializeApp} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey:"AIzaSyBpmWDkwcgkUWDASIaiuLNg7FuG2lWmXxU",
-    authDomain: "farma-80b4f.firebaseapp.com",
-    projectId: "farma-80b4f",
-    storageBucket: "farma-80b4f.appspot.com",
-    messagingSenderId: "679697403307",
-    appId: "1:679697403307:web:e191e2f6be14bc9d0f16ee"
-  };
-
+  apiKey: "AIzaSyBrRjuwpYeW-fSkuPNMuTdjq626tqI9ggY",
+  authDomain: "ph-farma.firebaseapp.com",
+  projectId: "ph-farma",
+  storageBucket: "ph-farma.appspot.com",
+  messagingSenderId: "204511010332",
+  appId: "1:204511010332:web:a251b58ca07ad7c15b89d9",
+  measurementId: "G-6MGNHYB64J"
+};
     // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   export const db = getFirestore(app);
